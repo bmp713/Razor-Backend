292dbc3cc84e7f2e2913fdc3948efec29b2e4a9e
unknown <bmp713@yahoo.com>
2025-06-22 20:50:23 -0700

    Frontend

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
 using Backend.Models; 
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,30 +14,46 @@ builder.Services.AddCors(
 var app = builder.Build();
 app.UseCors();
 
-var users = new List<User>();
-users.Add(new User
-{
-    id = new Random().Next(1000000000, int.MaxValue),
-    firstName = "Steve",
-    lastName = "Jobs",
-    email = "steve@apple.com",
-    password = "AAA"  
-});
-
-Console.WriteLine("User created: " + users[0]);
+// var users = new List<User>();
+// users.Add(new User
+// {
+//     id = new Random().Next(1000000000, int.MaxValue),
+//     firstName = "Steve",
+//     lastName = "Jobs",
+//     email = "steve@apple.com",
+//     password = "AAA"  
+// });
 
 app.MapGet("/users", () => {
-    Console.WriteLine("Get all users = > " + users);
-    return users;
+    string json = File.ReadAllText("Users.json");
+
+    var users = JsonSerializer.Deserialize<List<User>>(json);
+    foreach (var user in users){
+        Console.WriteLine(user);
+    }
+    
+    return users;   
 });
 
 app.MapPost("/users", (User user) => {
+    Console.WriteLine($"/users/{user.id}", user);
+
+    // Write JSON to file
+    string existingJson = File.ReadAllText("Users.json");
+    List<User> users = JsonSerializer.Deserialize<List<User>>(existingJson);
+
+    // Add the new user
     users.Add(user);
-    Console.WriteLine("User created: " + user);
+
+    // Write the entire updated array back to the file
+    File.WriteAllText("Users.json", JsonSerializer.Serialize(users, new JsonSerializerOptions { 
+        WriteIndented = true 
+    }));
+
+    //users.Add(user);
     return Results.Created($"/users/{user.id}", user);
 });
 
-// GET /users/{id} - Get user by ID
 // app.MapGet("/users/{id:int}", (int id) =>
 // {
 //     var user = users.FirstOrDefault(u => u.Id == id);
