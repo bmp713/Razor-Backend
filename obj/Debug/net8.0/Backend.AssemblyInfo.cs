78d41ed04ba3c33455708876346fc279d8398562
unknown <bmp713@yahoo.com>
2025-06-27 13:01:51 -0700

    Backend API

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0930b4bc54d9d49b338701659782047b8cb47bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a11a90a19725a2b07fc910f71c86f1b5ac3ad54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
